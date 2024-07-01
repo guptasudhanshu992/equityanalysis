98e53bbada658fb10021eaf53590628c214d7a7c
GuptaSudhanshu992 <135426798+GuptaSudhanshu992@users.noreply.github.com>
2024-07-01 16:13:11 +0530

    Production Settings updated
    Production Settings updated

--- equityanalysis/settings.py
+++ equityanalysis/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ej#l3q_d0rugm2+k)+!^ef1qhho4+dp&d!lg+zjb65f-yhy34&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
